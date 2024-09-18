70e5a406cc94a531fd92299acb79cdf03a9e57b5
mtss78 <mathis.mdjie@gmail.com>
2024-09-18 13:47:00 +0200

    envoie4

--- config/router.php
+++ config/router.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once (__DIR__ . "/function.php").
+require_once (__DIR__ . "/db.php");
 // Récupération de l'URI actuelle de la requête utilisateur
 // Cette partie extrait uniquement le chemin de l'URL (sans les paramètres GET ou les fragments)
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
